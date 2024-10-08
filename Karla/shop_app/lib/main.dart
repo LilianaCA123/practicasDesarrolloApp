832c29ef475bb26969fd9a65c2453fe33de39308
Karla40664 <kmartinez40664@ucq.edu.mx>
2024-10-07 20:06:04 -0600

    Agregando ejercicio 1

---
+++ Karla/shop_app/lib/main.dart
@@ -0,0 +1,17 @@
+
+void main() {
+  //Definimos constantes y variables
+  const tienda = "Karla Shop";
+  var producto;
+  int cantidad = 5;
+
+  //Imprimimos la constante
+  print("\nBienvenido a "+ tienda);
+
+  //Asignamos valores a las variables
+  producto = "Dulce de coco";
+
+
+  print("\nProducto "+ producto);
+  print("Cantidad ${cantidad}");
+}
